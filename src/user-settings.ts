71aaf6c769920548698028a7c5e3c3f8e8787a2d
Robin Dittmar <robindittmar@gmail.com>
2023-10-23 22:43:54 +0200

    feature: started implementing multiple views

---
+++ src/user-settings.ts
@@ -0,0 +1,28 @@
+
+export interface UserSettings {
+    selectedOrganization: string;
+    privateMode: boolean;
+}
+
+export function defaultUserSettings(): UserSettings {
+    return {
+        selectedOrganization: '',
+        privateMode: true,
+    };
+}
+
+export async function fetchUserSettings(): Promise<UserSettings> {
+    return await (await fetch('/api/user/me/settings')).json();
+}
+
+export async function updateUserSettings(userSettings: UserSettings): Promise<boolean> {
+    const result = await fetch('/api/user/me/settings', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userSettings),
+    });
+
+    return result.ok;
+}
---
+++ src/views/SettingsView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import type {UserSettings} from "@/user-settings";
+import OrganizationComponent from "@/components/OrganizationComponent.vue";
+
+const props = defineProps<{
+  userSettings: UserSettings;
+}>();
+
+defineEmits(['update-user-settings']);
+
+let settings = ref(props.userSettings);
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <div class="row mt-2">
+        <div class="col">
+          <OrganizationComponent />
+        </div>
+      </div>
+    </header>
+
+    <main>
+      <div class="row mt-2">
+        <div class="col">
+          <h1>Nutzer Einstellungen</h1>
+
+          <div class="form-check">
+            <input id="privateModeCheck" type="checkbox" class="form-check-input" v-model="settings.privateMode">
+            <label for="privateModeCheck" class="form-check-label">Privater Modus</label>
+          </div>
+
+          <button class="btn btn-primary" @click="$emit('update-user-settings', settings.value)">Speichern</button>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
