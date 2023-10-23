6afaffecfaf16c8d311a84c42caf6374a3798ea0
Robin Dittmar <robindittmar@gmail.com>
2023-10-23 22:46:43 +0200

    fix: api paths

--- src/user-settings.ts
+++ src/user-settings.ts
@@ -12,11 +12,11 @@ export function defaultUserSettings(): UserSettings {
 }
 
 export async function fetchUserSettings(): Promise<UserSettings> {
-    return await (await fetch('/api/user/me/settings')).json();
+    return await (await fetch('/api/users/me/settings')).json();
 }
 
 export async function updateUserSettings(userSettings: UserSettings): Promise<boolean> {
-    const result = await fetch('/api/user/me/settings', {
+    const result = await fetch('/api/users/me/settings', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
