9c5d4baf507e5a18fb0ded5a415b39a04fd4386f
Robin Dittmar <robindittmar@gmail.com>
2023-11-06 16:57:31 +0100

    add category now is client-side only until user chooses a name

--- src/components/CategoriesComponent.vue
+++ src/components/CategoriesComponent.vue
@@ -12,21 +12,31 @@ const emit = defineEmits(['update-categories']);
 
 const selectedCategory: Ref<number> = ref(0);
 const categoryCopy: Ref<Category> = ref({id: 0, name: ''});
+const newCategory: Ref<Category | undefined> = ref(undefined);
 
 const selectCategory = (categoryId: number) => {
   categoryCopy.value = {...props.categories.find(c => c.id === categoryId) ?? {id: 0, name: ''}};
   selectedCategory.value = categoryId;
 };
 
-const newCategory = async () => {
-  const newName = 'Unbenannt';
+const startNewCategory = () => {
+  newCategory.value = {
+    id: 0,
+    name: 'Unbenannt',
+  };
+};
 
+const cancelNewCategory = () => {
+  newCategory.value = undefined;
+};
+
+const insertCategory = async (category: Category) => {
   let result = await req('/api/categories', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({name: newName}),
+    body: JSON.stringify({name: category.name}),
   });
 
   if (result.ok) {
@@ -35,8 +45,7 @@ const newCategory = async () => {
     emit('update-categories');
     toast('Kategorie erfolgreich erstellt', ToastColor.Success);
 
-    categoryCopy.value = resp.category;
-    selectedCategory.value = resp.category.id;
+    newCategory.value = undefined;
   }
 };
 
@@ -91,7 +100,7 @@ const deleteCategory = async (category: Category) => {
                 <td>{{ i + 1 }}</td>
                 <td>
                   <div class="input-group">
-                    <input id="withdrawNote" class="form-control" type="text" maxlength="128" v-model="categoryCopy.name"/>
+                    <input id="categoryName" class="form-control" type="text" maxlength="128" v-model="categoryCopy.name"/>
                     <button class="btn btn-primary" @click="updateCategory(categoryCopy)"><i class="fa-solid fa-floppy-disk"></i></button>
                     <button class="btn btn-secondary" @click="selectCategory(0)"><i class="fa-solid fa-xmark"></i></button>
                     <button class="btn btn-danger" @click="deleteCategory(categoryCopy)"><i class="fa-solid fa-trash"></i></button>
@@ -106,12 +115,24 @@ const deleteCategory = async (category: Category) => {
               </tr>
             </template>
           </template>
+          <template v-if="newCategory">
+            <tr>
+              <td></td>
+              <td>
+                <div class="input-group">
+                  <input id="newCategoryName" class="form-control" type="text" maxlength="128" v-model="newCategory.name"/>
+                  <button class="btn btn-primary" @click="insertCategory(newCategory)"><i class="fa-solid fa-floppy-disk"></i></button>
+                  <button class="btn btn-secondary" @click="cancelNewCategory"><i class="fa-solid fa-xmark"></i></button>
+                </div>
+              </td>
+            </tr>
+          </template>
           </tbody>
         </table>
       </div>
     </div>
     <div class="d-flex justify-content-center pt-2 pb-2">
-      <button class="btn btn-success" @click="newCategory">
+      <button class="btn btn-success" @click="startNewCategory">
         <i class="fa-solid fa-plus"></i>&nbsp;Neu
       </button>
     </div>
