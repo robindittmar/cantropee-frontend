81297b13493d14be7d057125c04c1e96e11d4af8
Robin Dittmar <robindittmar@gmail.com>
2023-12-01 20:28:06 +0100

    slightly updated organization view

--- src/components/OrganizationComponent.vue
+++ src/components/OrganizationComponent.vue
@@ -36,47 +36,40 @@ watch(selectedOrg, async () => {
 
 <template>
   <div class="card">
-    <h2 class="card-title text-center mt-2">{{ user.currentOrganization?.name }}</h2>
     <div class="card-body">
       <div class="row">
         <div class="col">
-          <div class="mb-3">
-            <select id="organizationSelect" class="form-select" v-model="selectedOrg">
-              <option v-for="org in user.organizations" :key="org.id" :value="org.id"
-                      :selected="org.id === user.currentOrganization?.id">
-                {{ org.name }}
-              </option>
-            </select>
+          <div class="button-group mb-3 text-center">
+            <button class="btn btn-primary btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              {{ user.currentOrganization?.name }}
+            </button>
+            <ul class="dropdown-menu">
+              <li v-for="org in user.organizations" :key="org.id">
+                <a class="dropdown-item" @click="selectedOrg = org.id">{{ org.name }}</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
       <h4 class="text-center mt-5">Daueraufträge</h4>
-      <div class="row">
-        <div class="col">
-          <RecurringTransactionsComponent :display-values="!user.settings.privateMode"
-                                          :currency="user.currentOrganization?.currency ?? 'EUR'"
-                                          :show-taxes="user.currentOrganization?.usesTaxes ?? false"
-                                          :categories="categories"/>
-        </div>
+      <div class="card">
+        <RecurringTransactionsComponent :display-values="!user.settings.privateMode"
+                                        :currency="user.currentOrganization?.currency ?? 'EUR'"
+                                        :show-taxes="user.currentOrganization?.usesTaxes ?? false"
+                                        :categories="categories"/>
       </div>
       <template v-if="user.currentOrganization?.privileges?.includes('admin')">
         <h4 class="text-center mt-5">Kategorien</h4>
-        <div class="row">
-          <div class="col">
-            <CategoriesComponent :categories="categories" @update-categories="$emit('update-categories')"/>
-          </div>
+        <div class="card">
+          <CategoriesComponent :categories="categories" @update-categories="$emit('update-categories')"/>
         </div>
         <h4 class="text-center mt-5">Benutzer</h4>
-        <div class="row">
-          <div class="col">
-            <UsersComponent :user="user"/>
-          </div>
+        <div class="card">
+          <UsersComponent :user="user"/>
         </div>
         <h4 class="text-center mt-5">Rollen</h4>
-        <div class="row">
-          <div class="col">
-            <RolesComponent :user="user"/>
-          </div>
+        <div class="card">
+          <RolesComponent :user="user"/>
         </div>
       </template>
     </div>
